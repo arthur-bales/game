e066f15eebfd0ee5acf14f1566accd8afb91f489
Arthur <balesarthur@gmail.com>
2025-10-22 11:16:04 +0200

    Ajout de la barre statistique

--- script.js
+++ script.js
@@ -1,65 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
-let click = 0;
-let kamas = document.querySelector(".montant-kamas");
+  let click = 0;
+  let kamas = document.querySelector(".montant-kamas");
 
-const displayPointDeVie = document.querySelector(".point-de-vie");
-let hp = 1000;
-const mob = document.querySelector(".epouvantail");
-const displayResultAttaque = document.querySelector(".result-attaque");
-let attaque = 1;
+  const displayPointDeVie = document.querySelector(".point-de-vie");
+  let hp = 1000;
+  const mob = document.querySelector(".epouvantail");
+  const displayResultAttaque = document.querySelector(".result-attaque");
+  let attaque = 1;
 
-displayPointDeVie.innerText = hp + " / 1000";
+  displayPointDeVie.innerText = hp + " / 1000";
+  let statAttaque = document.querySelector(".stat-attaque");
+  statAttaque.innerText = "- Attaque : " + attaque;
 
+  // Button Buy --------------------------------------------
+  const buyTest = document.querySelector(".buy");
 
-// Button Buy --------------------------------------------
-const buyTest = document.querySelector(".buy"); 
-
-function buy() {
+  function buy() {
     click = click - 10;
     attaque++;
     console.log(click);
     kamas.innerText = click;
-    alert("+ 1 attaque")
-}
+    alert("+ 1 attaque");
 
-// Fonctionnalité GamePlay ------------------------------
-mob.addEventListener("click", () => {
-  if (hp > 0) {
-    hp = hp - attaque;
-    click++;
-    displayPointDeVie.innerText = hp + " / 1000";
-    displayResultAttaque.innerText = "- " + attaque;
-    displayResultAttaque.classList.add("show");
-    setTimeout(() => {
-      displayResultAttaque.classList.remove("show");
+    // Stat Attaque --------------------------------------
+    let statAttaque = document.querySelector(".stat-attaque");
+    statAttaque.innerText = "- Attaque : " + attaque;
+  }
+
+  // Fonctionnalité GamePlay ------------------------------
+  mob.addEventListener("click", () => {
+    if (hp > 0) {
+      hp = hp - attaque;
+      click++;
+      displayPointDeVie.innerText = hp + " / 1000";
+      displayResultAttaque.innerText = "- " + attaque;
+      displayResultAttaque.classList.add("show");
       setTimeout(() => {
-        displayResultAttaque.innerText = "\u00A0";
-      }, 100);
-    }, 200);
-    kamas.innerText = click;
+        displayResultAttaque.classList.remove("show");
+        setTimeout(() => {
+          displayResultAttaque.innerText = "\u00A0";
+        }, 100);
+      }, 200);
+      kamas.innerText = click;
     }
-  } );
+  });
 
-        buyTest.addEventListener("click", () => {
-        if (click >= 10){
-        buy();
-        } else {
-            alert("Pas assez de kamas")
-        }
-      });
+  buyTest.addEventListener("click", () => {
+    if (click >= 10) {
+      buy();
+    } else {
+      alert("⚠️ Pas assez de kamas ⚠️");
+    }
+  });
 
+  // Bouton shop -------------------------------------
+  const shop = document.querySelector(".shop");
+  const shopButton = document.querySelector(".deux");
+  const closeShopButton = document.querySelector(".closeShop");
+  shopButton.addEventListener("click", () => {
+    shop.style.display = "block";
+  });
 
-// Bouton shop -------------------------------------
-const shop = document.querySelector(".shop"); 
-const shopButton = document.querySelector(".deux");
-const closeShopButton = document.querySelector(".closeShop");
-shopButton.addEventListener("click", () => {
-  shop.style.display = "block";
-  console.log("teste")
-});
-
-closeShopButton.addEventListener("click", ()=> {
-    shop.style.display = "none"; 
-})
-
+  closeShopButton.addEventListener("click", () => {
+    shop.style.display = "none";
+  });
 });
